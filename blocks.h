40a0744b8399a96202f84f6e61520f7e8184f3d0
Fredrik Engstrand <fredrik@engstrand.nu>
2020-04-09 10:02:20 +0200

    Renamed kdeconnect-i3 to kdeconnect

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{ "", "separator", 0, 0 },
-	{ "", "kdeconnect-i3", 30, 0 },
+	{ "", "kdeconnect", 15, 0 },
 	{ "", "music", 0, 11 },
 	{ "", "wifistatus", 5, 0 },
 	{ "", "ethernet", 5, 0 },
