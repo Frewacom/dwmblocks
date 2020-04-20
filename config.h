2dd1f444fe97a8c4b1759503ad89dc5ceecb4d30
Johan Engstrand <johan@enstrand.nu>
2020-04-20 19:43:01 +0200

    Block settings, renamed config file

--- config.h
+++ config.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* { "", "separator", 0, 0 }, */
 	{ " ", "music", 0, 11 },
-	{ " ", "news", 15, 6},
+	{ " ", "news", 0, 6},
 	{ " ", "mailbox", 15, 12},
 	{ " ", "kdeconnect", 15, 0 },
 	{ " ", "wifistatus", 5, 0 },
